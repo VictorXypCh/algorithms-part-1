b00db8e45051b30c9e20cee07c73fca25fcdb4a2
VictorXypCh <victorxyp.ch@gmail.com>
2023-03-01 16:10:43 +0700

    Initial

---
+++ src/RandomWord.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+            if(!StdIn.isEmpty()){
+                StdOut.print(StdIn.readString());
+            } 
+    }
+}
